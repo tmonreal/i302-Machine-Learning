5b5381ffe27a9ff6c90280400ecf16a7d0b92eee
tmonreal <trinidadmonreal@gmail.com>
2024-08-09 15:15:05 -0300

    created datasets and template for TP2

---
+++ TPs/TP2/Resolucion_TP2/data_splitting.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+def train_val_split():
+    return
+
+def cross_val():
+    return
---
+++ TPs/TP2/Resolucion_TP2/metrics.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+def RMSE():
+    return
+
+def MAE():
+    return
+
+def R2():
+    return
